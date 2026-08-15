8248b9d1cf79b7122929c912c01897e1a0a28cc2
Link4Electronics <link4electronics@gmail.com>
2026-08-15 04:34:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/RSDKv3
+quick-sharun ./AppDir/bin/RSDKv3
 
 # Additional changes can be done in between here
 
