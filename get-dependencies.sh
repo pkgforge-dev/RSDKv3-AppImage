18aca384b364d94f0c04c37952ae1c62e0e317c5
Link4Electronics <link4electronics@gmail.com>
2026-08-15 20:54:17 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,10 +17,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package soniccd-git
-
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of RSDKv3 Decompilation..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/RSDKModding/RSDKv3-Decompilation"
