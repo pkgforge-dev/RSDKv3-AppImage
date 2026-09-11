6da796e08772f537f4a55baed0d4991e78d9a1c7
Link4Electronics <link4electronics@gmail.com>
2026-09-10 22:27:02 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,10 +7,10 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake     \
-    glew      \
-    libtheora \
-    sdl2
+    cmake       \
+    glew        \
+    libtheora   \
+    sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
